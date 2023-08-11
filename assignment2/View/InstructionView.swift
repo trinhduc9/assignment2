039dec142c824ff4a18536f3f92c11897e9324ac
Duc Trinh <89338794+trinhduc9@users.noreply.github.com>
2023-08-11 17:46:00 +0700

    a

---
+++ assignment2/View/InstructionView.swift
@@ -0,0 +1,25 @@
+/*
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2023B
+ Assessment: Assignment 2
+ Author: Trinh Van Minh Duc
+ ID: s3915177
+ Created  date: 11/08/2023
+ Last modified:
+ Acknowledgement:
+ */
+
+import SwiftUI
+
+struct InstructionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InstructionView_Previews: PreviewProvider {
+    static var previews: some View {
+        InstructionView()
+    }
+}
--- assignment2/assignment2App.swift
+++ assignment2/assignment2App.swift
@@ -8,6 +8,7 @@
  Created  date: 11/08/2023
  Last modified:
  Acknowledgement:
+- https://medium.com/@amitsrivastava115/toggle-between-dark-and-light-mode-in-swiftui-across-whole-app-5ccfc06a8eca
  */
 
 import SwiftUI
