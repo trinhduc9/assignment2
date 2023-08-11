b922ae8ed3afb525ad3debe67b3aeda1dd184744
Duc Trinh <89338794+trinhduc9@users.noreply.github.com>
2023-08-11 14:33:27 +0700

    Initial Commit

---
+++ assignment2/assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment2App.swift
+//  assignment2
+//
+//  Created by Duc Trinh Van Minh on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
