5665546b63e8f64afe11487a8897c63b3a09ca1d
Duc Trinh <89338794+trinhduc9@users.noreply.github.com>
2023-08-17 14:09:23 +0700

    datamanager

---
+++ assignment2/Model/DataManager.swift
@@ -0,0 +1,18 @@
+/*
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2023B
+ Assessment: Assignment 2
+ Author: Trinh Van Minh Duc
+ ID: s3915177
+ Created  date: 17/08/2023
+ Last modified:
+ Acknowledgement:
+ */
+import Foundation
+
+class DataManager: Codable {
+    
+    
+    func writeJSON
+}
